fd50b1167628bc7ec1c9acfa98f86926c5bfb503
kireque <5346530+kireque@users.noreply.github.com>
2024-01-30 10:59:39 +0100

    change folder structure

--- infrastructure/talos/cluster-0/apply.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -n 192.168.0.21 -f ./clusterconfig/cluster-0-delta.econline.local.yaml --insecure
-talosctl apply-config -n 192.168.0.22 -f ./clusterconfig/cluster-0-enigma.econline.local.yaml --insecure
-talosctl apply-config -n 192.168.0.23 -f ./clusterconfig/cluster-0-felix.econline.local.yaml --insecure
---
+++ kubernetes/main/talos/deploy-integrations.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# shellcheck disable=2312
+pushd integrations >/dev/null 2>&1 || exit 1
+
+touch cni/values.yaml
+rm -rf cni/charts
+envsubst < ../../apps/kube-system/cilium/app/values.yaml > cni/values.yaml
+kustomize build --enable-helm cni | kubectl apply -f -
+rm cni/values.yaml
+rm -rf cni/charts
+
+touch kubelet-csr-approver/values.yaml
+rm -rf kubelet-csr-approver/charts
+envsubst < ../../apps/system-controller/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
+if ! kubectl get ns system-controllers >/dev/null 2>&1; then
+  kubectl create ns system-controllers
+fi
+kustomize build --enable-helm kubelet-csr-approver | kubectl apply -f -
+rm kubelet-csr-approver/values.yaml
+rm -rf kubelet-csr-approver/charts
