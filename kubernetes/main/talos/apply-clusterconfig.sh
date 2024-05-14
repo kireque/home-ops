d79e1642a754a08734cdd116d31e2fd20fe1f09b
kireque <5346530+kireque@users.noreply.github.com>
2024-05-14 22:56:49 +0200

    overhaul

--- kubernetes/main/talos/apply-clusterconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -i -n 10.1.1.31 -f ./clusterconfig/main-delta.home.econline.nl.yaml
-talosctl apply-config -i -n 10.1.1.32 -f ./clusterconfig/main-enigma.home.econline.nl.yaml
-talosctl apply-config -i -n 10.1.1.33 -f ./clusterconfig/main-felix.home.econline.nl.yaml
