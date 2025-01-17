91fee6fd2af95b6dc67aefebc0638cd4908a9cdc
Kireque <5346530+kireque@users.noreply.github.com>
2025-01-17 11:06:51 +0100

    update path

--- kubernetes/main/apps/scripts/workload-scaler/app/scale-deployments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for ns in $(kubectl get ns --no-headers | cut -d " " -f1); do
-    # Check if there are deployments to scale
-    if kubectl get deployments -n $ns -l automaticSchedular=true --no-headers | grep -q '.'; then
-        echo "Scaling deployments in namespace '$ns'..."
-        kubectl scale deployments --namespace=$ns -l automaticSchedular=true --replicas=$1
-        echo "Deployments scaled in namespace '$ns'."
-    fi
-done
